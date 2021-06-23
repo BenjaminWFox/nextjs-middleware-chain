d94490167635ee8397514d7a84351de24ee7613e
Ben Fox <BenjaminWFox@gmail.com>
2021-06-22 17:16:51 -0700

    Add babel for transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,21 @@
 // rollup.config.js
+// import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: './build/main.js',
+      file: './dist/main.js',
       format: 'cjs'
     },
     {
-      file: './build/main.esm.js',
+      file: './dist/main.esm.js',
       format: 'esm'
     }
+  ],
+  plugins: [
+    // resolve(),
+    babel({ babelHelpers: 'bundled' })
   ]
 }
